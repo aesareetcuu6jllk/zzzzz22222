7620fe873c6be63e6bfe37ca3bcf4c2ec7558b28
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-02-08 22:44:03 +0300

    Update string.py

--- ArStringTep/modules/string.py
+++ ArStringTep/modules/string.py
@@ -231,7 +231,7 @@ async def string_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@Tepthon"))
+            await client(JoinChannelRequest("@HELLASUserBot"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -239,7 +239,7 @@ async def string_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("Tepthon")
+            await client.join_chat("HELLAS")
     except KeyError:
         pass
     try:
